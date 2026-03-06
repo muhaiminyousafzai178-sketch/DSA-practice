05237bcfafa6b83e15ed44ec4c7b70c8e0fa1271
muhaiminyousafzai178-sketch <muhaiminyousafzai178@gmail.com>
2026-03-07 00:52:37 +0500

    basic operators

---
+++ 01-basic-operators.js
@@ -0,0 +1,17 @@
+//  let a = 10 ;
+//  let b = 20 ;
+
+//  console.log (a+b)
+
+// let a = 10 
+// let b = 20
+ 
+// console.log (a+b + "is sum of 10 an 20 ")
+// Ans = 30 is sum od 10 an 20 
+
+// console.log ("2"+ 2)
+// Ans = 22 
+
+// console.log ("2"-2)
+// Ans = 0 
+
